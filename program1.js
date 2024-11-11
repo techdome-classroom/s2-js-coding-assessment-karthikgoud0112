4d1ddfb0891751f20212b38d53a13f10989f3ba8
Gangadhar Marka <markagangadhar7@gmail.com>
2024-11-11 15:47:20 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,11 +1,23 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isValid = function(s) {
-    
+    const bracketStack = [];
+    const closeToOpen = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let bracket of s) {
+        if (closeToOpen[bracket]) {
+            const topBracket = bracketStack.length === 0 ? '#' : bracketStack.pop();
+            if (topBracket !== closeToOpen[bracket]) {
+                return false;
+            }
+        } else {
+            bracketStack.push(bracket);
+        }
+    }
+
+    return bracketStack.length === 0;
 };
 
 module.exports = { isValid };
-
-
